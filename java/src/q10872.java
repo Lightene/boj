2e824a74f890a7974755df2ee6191733badbfe07
Lightene <lightene@naver.com>
2019-12-04 04:06:47 +0900

    java boj 10872 팩토리얼

---
+++ java/src/q10872.java
@@ -0,0 +1,18 @@
+//  10872 - 팩토리얼
+
+import java.util.Scanner;
+public class q10872{
+    public static int factorial(int value){
+        int result = 1;
+        for (int i=1; i<=value; i++){
+            result *= i;
+        }
+        return result;
+    }
+    public static void main (String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        System.out.println(factorial(N));
+    }
+}
