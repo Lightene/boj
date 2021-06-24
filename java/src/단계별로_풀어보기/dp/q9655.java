c065e319b7ad19b9b31ce7f54ccf04d60be4847d
lightene <lightene@naver.com>
2021-06-24 21:22:03 +0900

    update

---
+++ java/src/단계별로_풀어보기/dp/q9655.java
@@ -0,0 +1,20 @@
+package 단계별로_풀어보기.dp;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class q9655 {
+    public static void main(String[] args) throws Exception{
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(bf.readLine());
+
+        if(N%2 == 0){
+            System.out.println("CY");
+        }else{
+            System.out.println("SK");
+        }
+
+    }
+}
+
