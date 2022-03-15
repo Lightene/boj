e41d392e99aa85e244afd0ae55d9452b6bab4053
lightene <lightene@naver.com>
2022-03-15 18:13:45 +0900

    update

--- java/src/단계별로_풀어보기/그리디/q13164.java
+++ java/src/단계별로_풀어보기/그리디/q13164.java
@@ -4,39 +4,35 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.Collections;
 
 public class q13164 {
     public static void main(String[] args) throws IOException {
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-
         String input = bf.readLine();
+
         int N = Integer.parseInt(input.split(" ")[0]);
         int K = Integer.parseInt(input.split(" ")[1]);
-
-        int partition = N % K;
-        int range = N / K;
-        int temp = 0;
+        ArrayList<Integer> nIdx = new ArrayList<Integer>();
         int result = 0;
 
         ArrayList<Integer> student = new ArrayList<Integer>();
-        for(String Nx : bf.readLine().split(" ")){
-            student.add(Integer.parseInt(Nx));
+        for(String nX : bf.readLine().split(" ")){
+            student.add(Integer.parseInt(nX));
         }
 
-        for(int i = 0; i < range; i++){
-            result += student.get(temp) - student.get(partition - 1);
-            temp = partition + 1;
-            partition *= 2;
+        for(int i = 0; i < N-1; i++)
+        {
+            nIdx.add(student.get(i+1) - student.get(i));
         }
 
+        Collections.sort(nIdx);
 
-        for(int i = range * K; i < N; i++){
-            result += student.get(i);
+        for(int idx : nIdx.subList(0, N-K)){
+            result += idx;
         }
 
         System.out.println(result);
-
-
 
     }
 }
