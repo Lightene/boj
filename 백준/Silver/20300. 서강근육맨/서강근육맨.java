879d6c59c8ed685660ebd32979173182bb731d66
Lightene <Lightene@naver.com>
2023-02-18 02:07:44 +0900

    [Silver III] Title: 서강근육맨, Time: 184 ms, Memory: 17648 KB -BaekjoonHub

---
+++ 백준/Silver/20300. 서강근육맨/서강근육맨.java
@@ -0,0 +1,32 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(bf.readLine());
+        ArrayList<Long> M = new ArrayList<Long>();
+
+        long result = 0;
+
+        for(String str: bf.readLine().split(" ")){
+            M.add(Long.parseLong(str));
+        }
+
+        Collections.sort(M);
+
+        if(N % 2 != 0){
+            N--;
+            result = M.get(N-1);
+        }
+
+        for(int i = 0; i < N / 2; i ++){
+            result = Math.max(result, M.get(i) + M.get(N - i - 1));
+        }
+
+        System.out.println(result);
+    }
+}
