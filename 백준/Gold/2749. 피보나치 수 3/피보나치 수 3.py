275e6508a6b32647a5bc1977410ead1f8d393f65
Lightene <Lightene@naver.com>
2023-02-18 02:20:34 +0900

    [Gold II] Title: 피보나치 수 3, Time: 684 ms, Memory: 88084 KB -BaekjoonHub

---
+++ 백준/Gold/2749. 피보나치 수 3/피보나치 수 3.py
@@ -0,0 +1,11 @@
+N = int(input())
+N = N % 1500000
+
+lstNum = [0,1]
+
+cnt = 2
+
+for i in range(cnt,N+1):
+    lstNum.append(lstNum[i-1]+lstNum[i-2])
+    lstNum[i] = lstNum[i]%1000000
+print(lstNum[N])
