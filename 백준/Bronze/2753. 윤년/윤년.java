8a4fa28a8d9f61951807617ee17b16387f0c60e3
Lightene <Lightene@naver.com>
2023-05-10 01:19:57 +0900

    [Bronze V] Title: 윤년, Time: 132 ms, Memory: 14220 KB -BaekjoonHub

---
+++ 백준/Bronze/2753. 윤년/윤년.java
@@ -0,0 +1,15 @@
+import java.util.*;
+import java.io.*;
+
+public class Main{
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int input = Integer.parseInt(br.readLine());
+        
+        if(input % 4 == 0 && (input % 400 == 0 || input % 100 != 0)){
+            System.out.println(1);
+        }else{
+            System.out.println(0);
+        }
+    }
+}
